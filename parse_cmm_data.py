2cefe088000579d9dafa3ee28a8b2240019cb048
Shien Yang Lee <Shien Yang Lee>
2017-06-12 13:41:00 -0400

    Initial commit.

---
+++ parse_cmm_data.py
@@ -0,0 +1,7 @@
+def parse_cmm_data(file_path):
+	"""
+	Takes a path pointing to a text file in "Print" format from the Micro-Hite CMM.
+	Returns a dictionary with relevant flatness, parallelism, and distance measurements.
+	"""
+
+	
